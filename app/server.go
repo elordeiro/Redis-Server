4b7c6b4b61961de3605535af686229bccdf1ab89
elordeiro <estevao.lordeiro@hotmail.com>
2024-06-19 17:55:26 -0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -7,26 +7,45 @@ import (
 	"os"
 )
 
-func main() {
+type Server struct {
+	Listener net.Listener
+	Conn     []net.Conn
+}
+
+func NewServer() (*Server, error) {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		return nil, err
 	}
+	return &Server{
+		Listener: l,
+		Conn:     make([]net.Conn, 0),
+	}, nil
+}
 
-	fmt.Printf("listening %v\n", l.Addr())
-
-	conn, err := l.Accept()
+func (s *Server) serverAccept() {
+	conn, err := s.Listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		return
 	}
-	defer conn.Close()
+	s.Conn = append(s.Conn, conn)
 
+	go s.handleConnection(conn)
+}
+
+func (s *Server) serverClose() {
+	for _, conn := range s.Conn {
+		conn.Close()
+	}
+}
+
+func (s *Server) handleConnection(conn net.Conn) {
 	for {
 		buf := make([]byte, 1024)
 
-		_, err = conn.Read(buf)
+		_, err := conn.Read(buf)
 		if err != nil {
 			if err == io.EOF {
 				break
@@ -37,5 +56,20 @@ func main() {
 
 		conn.Write([]byte("+PONG\r\n"))
 	}
+}
 
+func main() {
+	server, err := NewServer()
+	if err != nil {
+		fmt.Println("Failed to create server")
+		os.Exit(1)
+	}
+
+	fmt.Println("listening on port 6379")
+
+	defer server.serverClose()
+
+	for {
+		server.serverAccept()
+	}
 }
